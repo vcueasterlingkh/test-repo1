29dd7351bd4582c7c11c9d13ab7fead255abae3f
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 13:18:55 -0500

    oops, I wasnt snake casing. now I am

--- testrepo1/one.py
+++ testrepo1/one.py
@@ -1,10 +1,10 @@
 
-def addTwo(one, two):
+def add_two(one, two):
     return one + two
 
-def addThree(one, two, three):
+def add_three(one, two, three):
     return one + two + three
 
 if __name__ == "__main__":
-    print(addTwo(5, 6))
-    print(addThree(5, 6, 7))
+    print(add_two(5, 6))
+    print(add_three(5, 6, 7))
--- tests/test-one.py
+++ tests/test-one.py
@@ -3,8 +3,8 @@ from testrepo1 import one
 
 class TestClass:
 
-    def testAddTwo_3_4(self):
+    def test_add_two_3_4(self):
         assert one.add_two(3, 4) == 7
 
-    def testAddThree_3_4_5(self):
+    def test_add_three_3_4_5(self):
         assert one.add_three(3, 4, 5) == 12
