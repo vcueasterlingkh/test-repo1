1f4a3da227169eeed1992486dbb07e89687a7650
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 14:07:18 -0500

    Updating with black

--- testrepo1/one.py
+++ testrepo1/one.py
@@ -1,14 +1,17 @@
 """This file has a few functions to do things with numbers."""
 
+
 @staticmethod
 def add_two(one, two):
     """Adds two values together and returns it."""
     return one + two
 
+
 @staticmethod
 def add_three(one, two, three):
     """Adds three values together and returns it."""
     return one + two + three
+
 
 if __name__ == "__main__":
     print(add_two(5, 6))
