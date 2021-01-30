f2b4fca84d8eeed41feabf05fdfd22a6dd270062
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 13:47:09 -0500

    adding finals?

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,13 +1,15 @@
+"""Test cases are important."""
 from testrepo1 import one
 
-
 class TestClass:
-    @staticmethod 
+"""This is some class for testing the things."""
+
+    @staticmethod
     def test_add_two_3_4():
         """Tests Add two values together and returns the values."""
         assert one.add_two(3, 4) == 7
 
-    @staticmethod 
+    @staticmethod
     def test_add_three_3_4_5():
         """Tests Add three values together and returns the values."""
         assert one.add_three(3, 4, 5) == 12
