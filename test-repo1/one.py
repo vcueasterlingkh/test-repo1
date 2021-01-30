bab95e16d5a1985e4de0453f2527fe10d8602bd0
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 12:18:38 -0500

    Fixing syntax of my python code

--- test-repo1/one.py
+++ test-repo1/one.py
@@ -2,5 +2,9 @@
 def add_two(one, two):
     return one + two
 
-def add_three(one, two, three)
+def add_three(one, two, three):
     return one + two + three
+
+if __name__ == "__main__":
+    print(add_two(5, 6))
+    print(add_three(5, 6, 7))
