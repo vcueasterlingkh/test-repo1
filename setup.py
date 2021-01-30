c2c8b0b7358d762436138f82053587170e8102ec
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 16:22:33 -0500

    adding docstring

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+"""Setup file to describe my project."""
+
 from setuptools import find_packages, setup
 
 
 setup(
-    name='testrepo1',
-    version='0.1',
-    description='Test repository',
-    author='Chris Fauerbach',
-    author_email='chfauerbach@vcu.edu',
-    package_dir={'': 'testrepo1'},
-    packages=find_packages(where='testrepo1'),
+    name="testrepo1",
+    version="0.1",
+    description="Test repository",
+    author="Chris Fauerbach",
+    author_email="chfauerbach@vcu.edu",
+    package_dir={"": "testrepo1"},
+    packages=find_packages(where="testrepo1"),
 )
