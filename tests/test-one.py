797bdfd5f4d7050e433768da16e367bd61243861
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-01-30 12:42:22 -0500

    working on my linting score

--- tests/test-one.py
+++ tests/test-one.py
@@ -3,8 +3,8 @@ from testrepo1 import one
 
 class TestClass:
 
-    def test_add_two_1(self):
+    def testAddTwo_3_4(self):
         assert one.add_two(3, 4) == 7
 
-    def test_add_three_1(self):
+    def testAddThree_3_4_5(self):
         assert one.add_three(3, 4, 5) == 12
