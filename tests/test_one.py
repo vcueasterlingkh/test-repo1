a2901ef14f8baf6677dbbdce6f3f466384fb9932
Chris Fauerbach <chrisfauerbach@gmail.com>
2021-02-02 19:25:33 -0500

    I made some changes in class.

--- tests/test_one.py
+++ tests/test_one.py
@@ -12,3 +12,8 @@ def test_add_two_3_4():
 def test_add_three_3_4_5():
     """Tests Add three values together and returns the values."""
     assert one.add_three(3, 4, 5) == 12
+
+
+def test_add_three_1_2_3():
+    """Tests add three values together and returns the values."""
+    assert one.add_three(1,2,3) == 6
